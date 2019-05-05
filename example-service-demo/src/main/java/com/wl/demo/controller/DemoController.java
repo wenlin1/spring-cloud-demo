011f008bcd525ecda5f1c47912a622dfdfd2e819
519628wenlin <wenlin>
2019-05-05 17:20:20 +0800

    提交修改

--- example-service-demo/src/main/java/com/wl/demo/controller/DemoController.java
+++ example-service-demo/src/main/java/com/wl/demo/controller/DemoController.java
@@ -6,9 +6,12 @@ import com.wl.demo.entity.DemoReq;
 import com.wl.demo.entity.DemoRes;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -21,14 +24,16 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController  {
+    private static Logger log = LoggerFactory.getLogger(DemoController.class);
 
     @ApiOperation(value = "测试输出demo字符串", notes = "返回msg以及自身的集群信息")
     @ApiParam(name = "msg", value = "get请求参数", required = true)
     @RequestMapping(value = "/hi" ,method = GET)
     public String hello(HttpServletRequest request, @RequestParam String msg){
         long startTime = System.currentTimeMillis();
-
-        String resBody = "I am the "+" , ";
+        
+        String resBody = "I am the "+msg+" , ";
+        log.info(resBody);
         return resBody;
     }
     @ApiOperation(value = "测试输出demo实体信息", notes = "根据demoReq返回demoRes信息")
@@ -43,6 +48,7 @@ public class DemoController  {
         resBody.setRetCode("000000");
         resBody.setRetDesc("SUCCESS");
         resBody.setRspBody(demoRes);
+        log.info(resBody.toString());
         return resBody;
     }
 
